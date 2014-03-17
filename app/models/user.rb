ab723865f3e136b644cf6c5c5631070579ccb284
josephktcheung <josephktcheung@gmail.com>
2014-03-17 12:09:04 +0800

    Add random password when password is not present

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,9 @@ class User
   field :salt, type: String
   field :fish, type: String
 
-  before_save :encrypt_password
+  before_save :set_random_password, :encrypt_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 50}, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 6 }
-  validate :check_password_and_password_confirmation
 
   embeds_many :items
 
@@ -32,6 +30,13 @@ class User
   protected
     def check_password_and_password_confirmation
       errors.add(:password_confirmation, "does not match password") unless self.password == self.password_confirmation
+    end
+
+    def set_random_password
+      if self.fish.blank? and password.blank?
+        self.salt = BCrypt::Engine.generate_salt
+        self.fish = BCrypt::Engine.hash_secret(SecureRandom.base64(32), self.salt)
+      end
     end
 
     def encrypt_password
