6dbbea7ef6aef0a3f295e244518acc99d6d5c532
josephktcheung <josephktcheung@gmail.com>
2014-03-20 00:11:18 +0800

    Add item edit, update, destroy action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,25 @@ class ItemsController < ApplicationController
     @item = current_user.items.create(name: params[:item][:name], description: params[:item][:description])
     redirect_to items_url, notice: "New wishlist item created!"
   end
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to items_url
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.delete
+    redirect_to items_url
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description, :image_url)
+    end
 end
